4af9cb9f5bb0c5c515934662fc49a263162d71ba
Dmarcela0 <79410611+Dmarcela0@users.noreply.github.com>
2025-11-06 06:44:54 -0500

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://proyectocuentabancaria-production.up.railway.app/api'
+  apiBaseUrl: 'https://proyectocuentabancaria-production.up.railway.app'
 };
